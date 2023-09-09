a3d018b313c61ee5d17993b1acf1a07d375610b0
aprokofyev2006 <aprokofyev2006@gmail.com>
2023-09-09 08:52:42 -0700

    Spring MVC part 2 changes in task 4-2

--- Spring-052-Review-Task-4/src/main/java/org/cydeo/service/OvertimeSalaryService.java
+++ Spring-052-Review-Task-4/src/main/java/org/cydeo/service/OvertimeSalaryService.java
@@ -10,13 +10,16 @@ public class OvertimeSalaryService {
 
     EmployeeRepository employeeRepository;
     HoursRepository hoursRepository;
+    HoursRepository hoursRepository2;
 
-    public OvertimeSalaryService(EmployeeRepository employeeRepository, @Qualifier("Overtime") HoursRepository hoursRepository) {
+    public OvertimeSalaryService(EmployeeRepository employeeRepository, @Qualifier("Overtime") HoursRepository hoursRepository, @Qualifier("Regular") HoursRepository hoursRepository2) {
         this.employeeRepository = employeeRepository;
         this.hoursRepository = hoursRepository;
+        this.hoursRepository2 = hoursRepository2;
     }
 
     public void calculateOvertimeSalary(){
+        System.out.println("Regular Salary: " + employeeRepository.getHourlyRate() * hoursRepository2.getHours());
         System.out.println("Salary: " + employeeRepository.getHourlyRate() * hoursRepository.getHours());
     }
 }
